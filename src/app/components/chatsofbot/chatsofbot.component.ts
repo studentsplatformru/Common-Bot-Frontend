2b8152d7e24ec675bbaa7b1d6f2807cde0c04bfd
VanillaTank <tedien2016@yandex.ru>
2020-10-30 10:13:20 +0800

    роутинг внешнего сайта

---
+++ src/app/components/chatsofbot/chatsofbot.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-chatsofbot',
+  templateUrl: './chatsofbot.component.html',
+  styleUrls: ['./chatsofbot.component.css']
+})
+export class ChatsofbotComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
