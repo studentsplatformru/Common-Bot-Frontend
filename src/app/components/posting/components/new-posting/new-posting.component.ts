8b4c1f8418574cbff215231ef4947147e6f8dc87
Alexandr <XoxlofAlexandr@yandex.ru>
2020-11-30 09:57:55 +0300

    add editor js

--- src/app/components/posting/components/new-posting/new-posting.component.ts
+++ src/app/components/posting/components/new-posting/new-posting.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
+import EditorJS from '@editorjs/editorjs';
+import Header from '@editorjs/header';
+import List from '@editorjs/list';
 
 @Component({
   selector: 'app-new-posting',
   templateUrl: './new-posting.component.html',
-  styleUrls: ['./new-posting.component.scss']
+  styleUrls: ['./new-posting.component.scss'],
 })
-export class NewPostingComponent implements OnInit {
+export class NewPostingComponent implements OnInit, AfterViewInit {
+  @ViewChild('editorJs') el: ElementRef;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit(): void {}
+
+  ngAfterViewInit(): void {
+    this.setEditorConfig();
   }
 
+  setEditorConfig(): void {
+    const editor = new EditorJS({
+      holder: this.el.nativeElement,
+      tools: {
+        header: Header,
+        list: List,
+      },
+    });
+  }
 }
