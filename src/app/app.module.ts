d53cd57f858d55e91db87bd3def4550a16495b35
egor <egorsasin@gmail.com>
2020-12-05 17:09:44 +0300

    Fix errors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,21 @@ import { MaterialModule } from './material.module';
 import { AppRoutingModule, routedComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
+import { BotbadgeComponent, SearchbotsComponent, SearchbotsortComponent, NavbarComponent } from './components';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
-import { NavBarComponent } from './components/nav-bar/nav-bar.component';
-
 @NgModule({
   declarations: [
     AppComponent,
     routedComponents,
     LoginComponent,
     RegisterComponent,
-    NavBarComponent,
+    NavbarComponent,
+    BotbadgeComponent,
+    SearchbotsortComponent,
+    SearchbotsComponent,
   ],
   imports: [
     ReactiveFormsModule,
---
+++ src/app/components/index.ts
@@ -0,0 +1,4 @@
+export { BotbadgeComponent } from './botbadge/botbadge.component';
+export { SearchbotsortComponent } from './searchbotsort/searchbotsort.component';
+export { SearchbotsComponent } from './searchbots/searchbots.component';
+export { NavbarComponent } from './navbar/navbar.component';
