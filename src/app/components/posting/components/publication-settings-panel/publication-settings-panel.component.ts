225b67031d48cbe478b9ca5174f2f56a8124770e
Alexandr <XoxlofAlexandr@yandex.ru>
2020-12-08 09:27:09 +0300

    add settings panel validation

--- src/app/components/posting/components/publication-settings-panel/publication-settings-panel.component.ts
+++ src/app/components/posting/components/publication-settings-panel/publication-settings-panel.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
-import { MatCheckboxChange } from '@angular/material/checkbox';
+import {
+  FormArray,
+  FormControl,
+  FormControlName,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { MatCheckbox, MatCheckboxChange } from '@angular/material/checkbox';
 import { MatRadioChange } from '@angular/material/radio';
 import { TelegramChat } from '../../models/telegram-chat.interface';
 
@@ -12,15 +18,36 @@ import { TelegramChat } from '../../models/telegram-chat.interface';
 export class PublicationSettingsPanelComponent implements OnInit {
   public isToggleAll: boolean = false;
   public counter: number = 3;
-  public dateOption: string | number = 1;
-  public deleteOption: string | number = 1;
   public showDateControls: boolean = false;
   public showDeleteControls: boolean = false;
-  public daySelected: FormControl = new FormControl(0);
-  public hourSelected: FormControl = new FormControl(1);
+  public settingsForm: FormGroup;
 
   public days: number[] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-  public hours: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  public hours: number[] = [
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+    10,
+    11,
+    12,
+    13,
+    14,
+    15,
+    16,
+    17,
+    18,
+    19,
+    20,
+    21,
+    22,
+    23,
+  ];
 
   public chats: TelegramChat[] = [
     {
@@ -53,10 +80,61 @@ export class PublicationSettingsPanelComponent implements OnInit {
 
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.createForm();
+    setInterval(() => {
+      console.log('this.settingsForm.valid:', this.settingsForm.valid);
+    }, 2000);
+  }
 
-  rofl(event: MatRadioChange): void {
-    console.log('event:', event.source.checked);
+  createForm(): void {
+    this.settingsForm = new FormGroup({
+      chats: new FormArray([], [Validators.required]),
+      dateControls: new FormControl('1'),
+      deleteControls: new FormControl('1'),
+      dateGroup: new FormGroup({
+        date: new FormControl(null),
+        time: new FormControl(null),
+      }),
+      deleteGroup: new FormGroup({
+        day: new FormControl(null),
+        hour: new FormControl(null),
+      }),
+    });
+  }
+
+  timeChange(flag: boolean): void {
+    const { date, time } = this.settingsForm.controls.dateGroup.controls;
+    if (flag) {
+      date.setValidators([Validators.required]);
+      time.setValidators([Validators.required]);
+      date.updateValueAndValidity();
+      time.updateValueAndValidity();
+      this.showDateControls = true;
+    } else {
+      date.setValidators(null);
+      time.setValidators(null);
+      date.updateValueAndValidity();
+      time.updateValueAndValidity();
+      this.showDateControls = false;
+    }
+  }
+
+  deleteChange(flag: boolean): void {
+    const { day, hour } = this.settingsForm.controls.deleteGroup.controls;
+    if (flag) {
+      day.setValidators([Validators.required]);
+      hour.setValidators([Validators.required]);
+      day.updateValueAndValidity();
+      hour.updateValueAndValidity();
+      this.showDeleteControls = true;
+    } else {
+      day.setValidators(null);
+      hour.setValidators(null);
+      day.updateValueAndValidity();
+      hour.updateValueAndValidity();
+      this.showDeleteControls = false;
+    }
   }
 
   toggle(chat: TelegramChat, event: MatCheckboxChange): void {
@@ -95,5 +173,26 @@ export class PublicationSettingsPanelComponent implements OnInit {
 
   showMore(): void {
     this.counter += 3;
+  }
+
+  checkboxChange(event, chat: TelegramChat): void {
+    const formArray: FormArray = this.settingsForm.controls[
+      'chats'
+    ] as FormArray;
+
+    if (event.checked) {
+      formArray.push(new FormControl(event.value));
+    } else {
+      let i: number = 0;
+
+      formArray.controls.forEach((ctrl: FormControl) => {
+        if (ctrl.value == event.value) {
+          formArray.removeAt(i);
+          return;
+        }
+
+        i++;
+      });
+    }
   }
 }
